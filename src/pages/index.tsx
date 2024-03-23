4d1dc39a555750d984d321cc139b838d898256e9
SantiagoVira <santiagovira06@gmail.com>
2024-03-22 21:46:17 -0400

    boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,7 +60,7 @@ function AuthShowcase() {
 
   const { data: secretMessage } = api.post.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined }
+    { enabled: sessionData?.user !== undefined },
   );
 
   return (
@@ -71,7 +71,9 @@ function AuthShowcase() {
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
+        onClick={
+          sessionData ? () => void signOut() : () => void signIn("google")
+        }
       >
         {sessionData ? "Sign out" : "Sign in"}
       </button>
--- src/server/db.ts
+++ src/server/db.ts
@@ -1,11 +1,24 @@
 import { PrismaClient } from "@prisma/client";
+// 1. Import libSQL and the Prisma libSQL driver adapter
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
 import { env } from "~/env";
+
+// 2. Instantiate libSQL
+const libsql = createClient({
+  url: env.TURSO_DATABASE_URL,
+  authToken: env.TURSO_AUTH_TOKEN,
+});
+
+// 3. Instantiate the libSQL driver adapter
+const adapter = new PrismaLibSQL(libsql);
 
 const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    adapter,
   });
 
 const globalForPrisma = globalThis as unknown as {
