a894ca7adce495f02d756e0ad99cfcc2ea4d27e0
SantiagoVira <santiagovira06@gmail.com>
2024-04-24 15:06:10 -0400

    organize /play

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,19 +1,38 @@
+import { forwardRef } from "react";
 import { cn } from "~/utils/cn";
 
-const Modal: React.FC<React.PropsWithChildren<{ className?: string }>> = ({
-  className = "",
-  children,
-}) => {
-  return (
+// const Modal: React.FC<React.PropsWithChildren<{ className?: string }>> = ({
+//   className = "",
+//   children,
+// }) => {
+//   return (
+//     <div
+//       className={cn(
+//         "bg-background absolute left-1/2 top-1/2 z-20 -translate-x-1/2 -translate-y-1/2 transform rounded-lg px-12 py-9 backdrop-blur-md",
+//         className,
+//       )}
+//     >
+//       {children}
+//     </div>
+//   );
+// };
+
+const Modal = forwardRef<
+  HTMLDivElement,
+  React.PropsWithChildren<{ className?: string }>
+>(({ className = "", children }, ref) => (
+  <div className="absolute left-0 top-0 flex h-screen w-screen items-center justify-center">
     <div
+      ref={ref}
       className={cn(
-        "bg-background absolute left-1/2 top-1/2 z-20 -translate-x-1/2 -translate-y-1/2 transform rounded-lg px-12 py-9 backdrop-blur-md",
+        "bg-background rounded-lg px-12 py-9 backdrop-blur-md",
         className,
       )}
     >
       {children}
     </div>
-  );
-};
+  </div>
+));
+Modal.displayName = "Modal";
 
 export default Modal;
--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,15 +1,18 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Button from "~/components/button";
 import Layout from "~/components/layout";
 import Link from "~/components/link";
 import Modal from "~/components/modal";
 import { api } from "~/utils/api";
+import autoAnimate from "@formkit/auto-animate";
 
 const Play: React.FC = () => {
   const sessionData = useSession();
   const router = useRouter();
+  const [joining, setJoining] = useState(false);
+  const parent = useRef<HTMLDivElement>(null);
 
   const createMutation = api.room.create.useMutation({
     onSuccess: async (r) => {
@@ -22,62 +25,77 @@ const Play: React.FC = () => {
   });
 
   const [code, setCode] = useState("");
+  useEffect(() => {
+    parent.current && autoAnimate(parent.current);
+  }, [parent]);
 
-  if (sessionData.status === "unauthenticated") {
-    void router.push("/");
-    return <></>;
-  } else if (sessionData.status === "loading") {
-    return <></>;
-  }
+  //   if (sessionData.status === "unauthenticated") {
+  //     void router.push("/");
+  //     return <></>;
+  //   } else if (sessionData.status === "loading") {
+  //     return <></>;
+  //   }
 
   const joinRoom = async () => {
     // CHECK IF CODE EXISTS
-
     await router.push(`/room/${code.slice(1)}`);
   };
 
   return (
     <Layout title="Play">
-      <Modal className="flex flex-col items-center gap-4">
+      <Modal className="flex min-w-80 flex-col items-center gap-4" ref={parent}>
         <h2 className="text-2xl font-semibold">JOIN A GAME</h2>
-        <input
-          type="text"
-          placeholder="#ABCD"
-          value={code}
-          onChange={(e) => {
-            let val = e.currentTarget.value;
-            if (!val.startsWith("#")) {
-              val = "#" + val;
-              if (val.length === 6) {
-                val = val.slice(0, 5);
+        {joining && (
+          <input
+            type="text"
+            placeholder="#ABCD"
+            value={code}
+            onChange={(e) => {
+              let val = e.currentTarget.value;
+              if (!val.startsWith("#")) {
+                val = "#" + val;
+                if (val.length === 6) {
+                  val = val.slice(0, 5);
+                }
               }
-            }
-            setCode(val.toUpperCase());
-          }}
-          onKeyDown={async (e) => {
-            if (e.key === "Enter") await joinRoom();
-          }}
-          onBlur={(e) => {
-            if (e.currentTarget.value === "#") setCode("");
-          }}
-          maxLength={5}
-          className="w-64 rounded-lg border border-text bg-transparent p-3 text-center text-3xl font-bold"
-        />
+              setCode(val.toUpperCase());
+            }}
+            onKeyDown={async (e) => {
+              if (e.key === "Enter") await joinRoom();
+            }}
+            onBlur={(e) => {
+              if (e.currentTarget.value === "#") setCode("");
+            }}
+            maxLength={5}
+            className="border-text w-64 rounded-lg border bg-transparent p-3 text-center text-3xl font-bold"
+          />
+        )}
+
         <Button
           className="w-full text-xl font-semibold"
-          disabled={code.length != 5}
-          onClick={joinRoom}
+          disabled={joining && code.length != 5}
+          onClick={joining ? joinRoom : () => setJoining(true)}
         >
-          GO!
+          {joining ? "Go!" : "Join a room"}
         </Button>
-        <Button
-          className="w-full text-xl font-semibold"
-          onClick={async () => {
-            await createMutation.mutateAsync();
-          }}
-        >
-          Create a room
-        </Button>
+        {!joining && (
+          <>
+            <div className="justify-apart flex w-full items-center gap-2">
+              <hr className="border-text w-full" />
+              <p>OR</p>
+              <hr className="border-text w-full" />
+            </div>
+            <Button
+              className="w-full text-xl font-semibold"
+              onClick={async () => {
+                await createMutation.mutateAsync();
+              }}
+            >
+              Create a room
+            </Button>
+          </>
+        )}
+
         <Link href="/how-to" variant="underlined">
           Learn to play →
         </Link>
