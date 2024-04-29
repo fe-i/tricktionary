c136a58778c7fba564a9d1267b584c95389c1638
fe-i <feic47@gmail.com>
2024-04-29 10:55:52 -0400

    quantity param in word api

--- src/pages/api/word.ts
+++ src/pages/api/word.ts
@@ -2,26 +2,31 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 import * as cheerio from "cheerio";
 
-export const dynamic = "force-dynamic";
-
 const Word = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getSession({ req });
+  const { quantity } = req.query;
 
   if (session) {
-    const response = await fetch("https://randomword.com", {
-      headers: {
-        "User-Agent": Date.now().toFixed(), // USED TO GET NEW WORDS ON RELOAD
-      },
-    });
+    const words = [];
 
-    const html = await response.text();
-    const $ = cheerio.load(html);
-    const data = $(".section #shared_section")
-      .text()
-      .trim()
-      .split("\n\t\t\t\t");
+    for (let i = 0; i < Number(quantity || 1); i++) {
+      const response = await fetch("https://randomword.com", {
+        headers: {
+          "User-Agent": Date.now().toFixed(), // USED TO GET NEW WORDS ON RELOAD
+        },
+      });
 
-    return res.status(200).json({ word: data[0], definition: data[1] });
+      const html = await response.text();
+      const $ = cheerio.load(html);
+      const data = $(".section #shared_section")
+        .text()
+        .trim()
+        .split("\n\t\t\t\t");
+
+      words.push({ word: data[0], definition: data[1] });
+    }
+
+    return res.status(200).send(JSON.stringify(words, null, 2));
   }
   return res.status(401).send({ error: 401, message: "Unauthorized." });
 };
