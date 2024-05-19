292816ae10346864b773233cb59b5b39fd0d6990
fe-i <114176049+fe-i@users.noreply.github.com>
2024-05-19 17:43:57 -0400

    implement profile page mostly

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -5,15 +5,20 @@ import { api } from "~/utils/api";
 import { useToast } from "~/components/use-toast";
 import Modal from "~/components/modal";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/avatar";
-import UnderlineHover from "~/components/underline-hover";
 import Button from "~/components/button";
+import { useState } from "react";
+import UnderlineHover from "~/components/underline-hover";
 
-const Play: React.FC = () => {
+const Profile: React.FC = () => {
   // HOOKS
   const sessionData = useSession();
   const router = useRouter();
   const { toast } = useToast();
+  const [editing, setEditing] = useState(false);
+
+  // QUERIES & MUTATIONS
   const { data: profileData } = api.user.findUnique.useQuery();
+  const updateMutation = api.user.update.useMutation();
 
   // AUTHENTICATION
   if (sessionData.status === "unauthenticated") {
@@ -22,6 +27,8 @@ const Play: React.FC = () => {
   } else if (sessionData.status === "loading") {
     return <></>;
   }
+
+  if (!profileData) return <></>;
 
   return (
     <Layout title="Profile">
@@ -32,18 +39,56 @@ const Play: React.FC = () => {
           )}
           <AvatarFallback>{sessionData.data?.user.name?.at(0)}</AvatarFallback>
         </Avatar>
-        <UnderlineHover>
-          <h1 className="font-bold">{sessionData.data?.user.name}</h1>
-        </UnderlineHover>
-        <p>&quot;Title&quot;</p>
-        <p>Games Played: {profileData?.gamesPlayed}</p>
-        <p>Games Won: {profileData?.gamesWon}</p>
-        <p>High Score: {profileData?.highScore}</p>
-        {/**todo: display stats better */}
-        <Button className="w-full">Edit Info</Button>
+        <div className="flex flex-col text-center">
+          {editing ? (
+            <input
+              className="text-md w-full rounded border border-text bg-background p-2 outline-none"
+              placeholder="Name"
+              maxLength={64}
+              onBlur={async (e) => {
+                const val = e.currentTarget.value;
+                if (val.length < 3) return;
+                await updateMutation.mutateAsync({ name: val }).then(
+                  () =>
+                    void toast({
+                      title: "Changes saved!",
+                      description: "Profile successfully updated",
+                    }),
+                );
+              }}
+            />
+          ) : (
+            <UnderlineHover>
+              <h1 className="font-bold">{sessionData.data?.user.name}</h1>
+            </UnderlineHover>
+          )}
+          <h2 className="text-lg">&quot;Title&quot;</h2>
+        </div>
+        <hr />
+        <div className="flex w-full flex-col gap-2">
+          <Stat title="Games Won" value={profileData.gamesWon} />
+          <Stat title="Games Played" value={profileData.gamesPlayed} />
+          <Stat title="High Score" value={profileData.highScore} />
+        </div>
+        <Button
+          className="w-full"
+          variant={editing ? "gray" : "primary"}
+          onClick={() => setEditing(!editing)}
+        >
+          {editing ? "Cancel" : "Edit Info"}
+        </Button>
       </Modal>
     </Layout>
   );
 };
 
-export default Play;
+export default Profile;
+
+const Stat: React.FC<{ title: string; value: number }> = ({ title, value }) => {
+  return (
+    <div className="flex flex-col">
+      <h3 className="text-lg font-bold">{title}</h3>
+      <h3>{value.toLocaleString()}</h3>
+    </div>
+  );
+};
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const userRouter = createTRPCRouter({
@@ -9,4 +10,17 @@ export const userRouter = createTRPCRouter({
       where: { id: ctx.session.user.id },
     });
   }),
+
+  update: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db.user.update({
+        where: { id: ctx.session.user.id },
+        data: { ...input },
+      });
+    }),
 });
